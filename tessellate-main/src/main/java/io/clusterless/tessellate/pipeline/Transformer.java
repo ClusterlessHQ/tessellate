0f1f3ab69ffd78cb98544c71b55b3101b273e224
Chris K Wensel <chris@wensel.net>
2025-04-02 11:35:31 -0700

    use SWAP to replace arguments with non-argument and operation result values

--- tessellate-main/src/main/java/io/clusterless/tessellate/pipeline/Transformer.java
+++ tessellate-main/src/main/java/io/clusterless/tessellate/pipeline/Transformer.java
@@ -54,9 +54,10 @@ public class Transformer {
 
         if (operation.exp() != null) {
             IntrinsicBuilder.Result result = create(context, operation);
-            // this overcomes what seems to be a bug with REPLACE
-            Fields selector = result.arguments().isAll() || (context.currentFields.contains(result.arguments()) && context.currentFields.size() == result.arguments().size()) ? Fields.RESULTS : Fields.REPLACE;
+            // SWAP allows for efficient replacement of arguments with non-argument and results
+            Fields selector = result.arguments().isAll() || (context.currentFields.contains(result.arguments()) && context.currentFields.size() == result.arguments().size()) ? Fields.RESULTS : Fields.SWAP;
             Pipe pipe = new Each(context.pipe, result.arguments(), result.function(), selector);
+
             Fields currentFields = context.currentFields.subtract(result.arguments()).append(result.results());
             return context.update(currentFields, pipe);
         }
